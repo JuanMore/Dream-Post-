b079603262015dc6deac1b3e3da9c2644b009710
JuanMore <morenostudent1@gmail.com>
2021-09-14 20:27:02 -0700

    updated files

--- app.js
+++ app.js
@@ -35,6 +35,6 @@ app.get('/', async(req, res) => {
 app.use('/articles', articleRouter)
 
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Listening on port')
 })
