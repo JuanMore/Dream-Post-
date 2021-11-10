eb06fe1b93fee5207d58a100fdb1def3c2fd372b
Juan Moreno <juanmoreno@Juans-MacBook-Pro.local>
2021-11-10 01:21:35 -0800

    deploy attempt 1

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+const dotenv = require('dotenv')
+dotenv.config({
+    path: __dirname + '/.env'
+});
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const Article = require('./models/articles')
 const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
-
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/DreamPost'
 // const path = require('path')
-
-mongoose.connect('mongodb://localhost:27017/DreamPost', {
+// 'mongodb://localhost:27017/DreamPost'
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
@@ -19,20 +23,28 @@ db.once('open', () => {
 })
 
 app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({
+    extended: false
+}))
 app.use(methodOverride('_method')) //override the method
 
 
 // allows us to access our diff params from articles form inside out route
 
-app.get('/', async(req, res) => {
+app.get('/', async (req, res) => {
     // init variable to = our article reqs
-    const articles = await Article.find().sort({ createdAt: 'desc'})
+    const articles = await Article.find().sort({
+        createdAt: 'desc'
+    })
     // pass our articles to index by passing object to our render
-    res.render('articles/index', {articles: articles})
+    res.render('articles/index', {
+        articles: articles
+    })
 })
 
 app.use('/articles', articleRouter)
 
 
 app.listen(process.env.PORT || 3000)
+
+console.log(dbUrl);
