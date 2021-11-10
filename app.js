2e03ee5ce8a8250b708e722e09ffffca8f08b6a6
Juan <morenostudent1@gmail.com>
2021-11-10 01:29:05 -0800

    deployment

--- app.js
+++ app.js
@@ -9,14 +9,8 @@ const Article = require('./models/articles')
 const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/DreamPost'
-// const path = require('path')
-<<<<<<< HEAD
-// 'mongodb://localhost:27017/DreamPost'
-mongoose.connect(dbUrl, {
-=======
 
-mongoose.connect('mongodb+srv://Moreno-Dev:<>@cluster0.6apug.mongodb.net/DreamPost', {
->>>>>>> 6242dc0e26a58e68fa7e66a0be05ae8d83f79c76
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
